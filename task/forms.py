9c431ff9db674b565b1fd2c6d6d850fecb12cf76
ChiquiPR <johansteven.h10@gmail.com>
2022-12-08 10:30:43 -0500

    task complete

--- task/forms.py
+++ task/forms.py
@@ -1,10 +1,14 @@
 
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
-    
+        widgets = {
+            'title' : forms.TextInput(attrs={'class': 'form-control'}),
+            'description' : forms.Textarea(attrs={'class': 'form-control'}),
+            'important' : forms.CheckboxInput(attrs={'class': 'form-check_input'})
+        }
     
